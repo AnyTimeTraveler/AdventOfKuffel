aa776fe18ab7d6b9b0ba7974c8c2548ba30511d6
Simon Struck <simon@simonscode.org>
2022-12-04 20:30:14 +0100

    Day 4 complete :)

---
+++ src/main/java/org/simonscode/Day04b.java
@@ -0,0 +1,40 @@
+package org.simonscode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day04b {
+
+    public static void main(String[] args) throws Exception {
+        File input = new File("input/04");
+        BufferedReader br = new BufferedReader(new FileReader(input));
+        Pattern pattern = Pattern.compile("(\\d+)-(\\d+),(\\d+)-(\\d+)");
+        int points = 0;
+
+        while (br.ready()) {
+            String line = br.readLine();
+            Matcher matcher = pattern.matcher(line);
+            if (!matcher.matches()) {
+                continue;
+            }
+            int[] numbers = {Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4)),};
+
+            if (numbers[0] == numbers[2] || numbers[1] == numbers[3]) {
+                points++;
+            } else if (numbers[0] < numbers[2]) {
+                if (numbers[1] >= numbers[2]) {
+                    points++;
+                }
+            } else {
+                if (numbers[3] >= numbers[0]) {
+                    points++;
+                }
+            }
+        }
+
+        System.out.println("Points: " + points);
+    }
+}
